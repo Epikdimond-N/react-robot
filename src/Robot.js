f3d9deaa67c64adfe17e43a28c9e609899f4827d
Epikdimond-N <nathanael.pivot@ynov.com>
2024-10-27 10:55:00 +0100

    update README

---
+++ src/Robot.js
@@ -0,0 +1,13 @@
+// src/Home.js
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/ebCKW1G53D7WHMIR/scene.splinecode" 
+      />
+    </main>
+  );
+}
